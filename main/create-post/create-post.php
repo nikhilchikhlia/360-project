3de2764bca141670febfbdc8c12043dea73163d2
Ishaan Gupta <ishaangupta02@gmail.com>
2024-03-23 16:56:25 -0700

    Update create-post.php

--- main/create-post/create-post.php
+++ main/create-post/create-post.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if the user is logged in
 
-if(isset($_SESSION['loggedin']) && $_SESSION[loggedin] == true) {
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
 } else {
     heading("Location: login-message.html");
